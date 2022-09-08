0ac583ad9a339a152920516e7d84709eb7b648df
Yevhen Malyshko <memx86@gmail.com>
2022-09-08 21:35:14 +0300

    basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,52 @@
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+const socket = io("localhost:3002/");
 const App = () => {
-  return <div>App</div>;
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [horses, setHorses] = useState([]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
+
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    socket.on("ticker", (data) => {
+      setHorses(data);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("ticker");
+    };
+  }, []);
+
+  const handleStart = () => {
+    socket.emit("start");
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleStart}>
+        Start
+      </button>
+      {isConnected && (
+        <ul>
+          {horses.map(({ name, distance }) => (
+            <li key={name}>
+              <p>{name}</p>
+              <p>{distance}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default App;
